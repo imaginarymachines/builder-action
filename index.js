494e1889e28f0fe2a65ca71d9b3610580bdd46d3
Josh Pollock <jpollock412@gmail.com>
2022-03-11 16:52:14 -0500

    add some debug logging

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const core = require("@actions/core");
+const exec = require("@actions/exec");
+const github = require("@actions/github");
+
 const pluginMachine = require("plugin-machine").default;
 
 // most @actions toolkit packages have async methods
 async function run() {
 	const { createDockerApi, builder } = pluginMachine;
 	const pmDockerApi = await createDockerApi({});
-	const pluginDir = __dirname;
+	const pluginDir = exec.exec("pwd");
+	core.debug(pluginDir);
+	const payload = JSON.stringify(github.context.payload, undefined, 2);
+	core.debug(`The event payload: ${payload}`);
+
 	const pluginMachineJson = pluginMachine.getPluginMachineJson(
 		pluginDir,
 		//Optional ovverides
