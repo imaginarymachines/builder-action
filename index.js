c81723b95e4e875170bab9878b293c4a01412cc1
Josh Pollock <jpollock412@gmail.com>
2022-05-09 11:40:47 -0400

    run with path

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
+const core = require('@actions/core');
+import {which} from "@actions/io"
+const exec = require('@actions/exec');
 
-// most @actions toolkit packages have async methods
+
+const runCommand = async ({path, args = [],options = {}}) => {
+	let output = '';
+	let error = '';
+
+	options.listeners = {
+	  stdout: (data) => {
+		output += data.toString();
+	  },
+	  stderr: (data) => {
+		error += data.toString();
+	  }
+	};
+	return new Promise(async(resolve, reject) => {
+		await exec.exec(path, args, options);
+		if(error){
+			reject(error);
+		}
+		resolve(output);
+	});
+
+}
 async function run() {
-	const core = require('@actions/core')
-	const glob = require('@actions/glob')
+  const paths = {
+    npm: await which("npm", true),
+    npx: await which("npx", true),
+    yarn: await which("yarn", true),
+  };
 
-	const globOptions = {
-		followSymbolicLinks: 'TRUE'
-	}
-	const globber = glob.create(core.getInput('files'), globOptions)
-	for await (const file of globber.globGenerator()) {
-	console.log(file)
-	}
+  const token = core.getInput('token');
+  await runCommand({path:paths.npm, args:["install"]});
 
+  await runCommand({path:paths.npx, args:["install plugin-machine -g"]});
 }
 
 run();
