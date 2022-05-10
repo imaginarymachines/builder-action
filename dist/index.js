41ac5bc53c83aee8eed19cae673ad3e4f39e7d78
Josh Pollock <jpollock412@gmail.com>
2022-05-10 19:29:12 -0400

    console.log({upload});

--- dist/index.js
+++ dist/index.js
@@ -3039,59 +3039,59 @@ const runCommand = async ({path, args = [],options = {}}) => {
 
 }
 async function run() {
-  const paths = {
-    npm: await (0,_actions_io__WEBPACK_IMPORTED_MODULE_0__.which)("npm", true),
-    npx: await (0,_actions_io__WEBPACK_IMPORTED_MODULE_0__.which)("npx", true),
-    yarn: await (0,_actions_io__WEBPACK_IMPORTED_MODULE_0__.which)("yarn", true),
-  };
-
-  const token = core.getInput('token');
-  let pluginDir = core.getInput('pluginDir');
-  if( ! pluginDir ){
-	pluginDir = process.env.GITHUB_WORKSPACE;
-  }
-  const buildDir = 'output';
-  const pluginDirArg = `--pluginDir=${pluginDir}`;
-  const tokenArg = `--token=${token}`;
-  const buildDirArg = `--buildDir=${buildDir}`;
-  const pluginMachine = async (args) => {
-	return await runCommand({
-		path:paths.npx,
-		args:["plugin-machine", ...args, pluginDirArg, tokenArg]
-	});
-  }
-  await runCommand({path:paths.npm, args:["install","plugin-machine","-g"]});
-  await pluginMachine(["plugin","build",buildDirArg]);
-  await pluginMachine(["plugin","zip",buildDirArg]);
-  const uploader = async () => {
-	let url = ''
-	const listeners = {
-		stdout: (data) => {
-			data = data.toString();
-			if( data.startsWith('Upload completed')){
-				url = data.replace('Upload completed', '').trim();
-			}
-			if( data.startsWith('\u001b[32mUpload completed')){
-				url = data.replace('\u001b[32mUpload completed','').trim();
-			}
-
-		},
-		stderr: (data) => {
-
-		}
+	const paths = {
+		npm: await (0,_actions_io__WEBPACK_IMPORTED_MODULE_0__.which)("npm", true),
+		npx: await (0,_actions_io__WEBPACK_IMPORTED_MODULE_0__.which)("npx", true),
+		yarn: await (0,_actions_io__WEBPACK_IMPORTED_MODULE_0__.which)("yarn", true),
 	};
-	await runCommand({
-		path:paths.npx,
-		args:["plugin-machine", "upload", pluginDirArg, tokenArg],
-		options: {
-			listeners
-		}
-	});
-	return url;
-  }
-  const upload = await uploader();
-  upload.forEach( line => console.log(line));
-  core.setOutput('upload', upload);
+
+	const token = core.getInput('token');
+	let pluginDir = core.getInput('pluginDir');
+	if( ! pluginDir ){
+		pluginDir = process.env.GITHUB_WORKSPACE;
+	}
+	const buildDir = 'output';
+	const pluginDirArg = `--pluginDir=${pluginDir}`;
+	const tokenArg = `--token=${token}`;
+	const buildDirArg = `--buildDir=${buildDir}`;
+	const pluginMachine = async (args) => {
+		return await runCommand({
+			path:paths.npx,
+			args:["plugin-machine", ...args, pluginDirArg, tokenArg]
+		});
+	}
+	await runCommand({path:paths.npm, args:["install","plugin-machine","-g"]});
+	await pluginMachine(["plugin","build",buildDirArg]);
+	await pluginMachine(["plugin","zip",buildDirArg]);
+	const uploader = async () => {
+		let url = ''
+		const listeners = {
+			stdout: (data) => {
+				data = data.toString();
+				if( data.startsWith('Upload completed')){
+					url = data.replace('Upload completed', '').trim();
+				}
+				if( data.startsWith('\u001b[32mUpload completed')){
+					url = data.replace('\u001b[32mUpload completed','').trim();
+				}
+
+			},
+			stderr: (data) => {
+
+			}
+		};
+		await runCommand({
+			path:paths.npx,
+			args:["plugin-machine", "upload", pluginDirArg, tokenArg],
+			options: {
+				listeners
+			}
+		});
+		return url;
+	}
+  	const upload = await uploader();
+	console.log({upload});
+  	core.setOutput('upload', upload);
 
 }
 
--- index.js
+++ index.js
@@ -26,59 +26,59 @@ const runCommand = async ({path, args = [],options = {}}) => {
 
 }
 async function run() {
-  const paths = {
-    npm: await which("npm", true),
-    npx: await which("npx", true),
-    yarn: await which("yarn", true),
-  };
-
-  const token = core.getInput('token');
-  let pluginDir = core.getInput('pluginDir');
-  if( ! pluginDir ){
-	pluginDir = process.env.GITHUB_WORKSPACE;
-  }
-  const buildDir = 'output';
-  const pluginDirArg = `--pluginDir=${pluginDir}`;
-  const tokenArg = `--token=${token}`;
-  const buildDirArg = `--buildDir=${buildDir}`;
-  const pluginMachine = async (args) => {
-	return await runCommand({
-		path:paths.npx,
-		args:["plugin-machine", ...args, pluginDirArg, tokenArg]
-	});
-  }
-  await runCommand({path:paths.npm, args:["install","plugin-machine","-g"]});
-  await pluginMachine(["plugin","build",buildDirArg]);
-  await pluginMachine(["plugin","zip",buildDirArg]);
-  const uploader = async () => {
-	let url = ''
-	const listeners = {
-		stdout: (data) => {
-			data = data.toString();
-			if( data.startsWith('Upload completed')){
-				url = data.replace('Upload completed', '').trim();
-			}
-			if( data.startsWith('\u001b[32mUpload completed')){
-				url = data.replace('\u001b[32mUpload completed','').trim();
-			}
-
-		},
-		stderr: (data) => {
-
-		}
+	const paths = {
+		npm: await which("npm", true),
+		npx: await which("npx", true),
+		yarn: await which("yarn", true),
 	};
-	await runCommand({
-		path:paths.npx,
-		args:["plugin-machine", "upload", pluginDirArg, tokenArg],
-		options: {
-			listeners
-		}
-	});
-	return url;
-  }
-  const upload = await uploader();
-  upload.forEach( line => console.log(line));
-  core.setOutput('upload', upload);
+
+	const token = core.getInput('token');
+	let pluginDir = core.getInput('pluginDir');
+	if( ! pluginDir ){
+		pluginDir = process.env.GITHUB_WORKSPACE;
+	}
+	const buildDir = 'output';
+	const pluginDirArg = `--pluginDir=${pluginDir}`;
+	const tokenArg = `--token=${token}`;
+	const buildDirArg = `--buildDir=${buildDir}`;
+	const pluginMachine = async (args) => {
+		return await runCommand({
+			path:paths.npx,
+			args:["plugin-machine", ...args, pluginDirArg, tokenArg]
+		});
+	}
+	await runCommand({path:paths.npm, args:["install","plugin-machine","-g"]});
+	await pluginMachine(["plugin","build",buildDirArg]);
+	await pluginMachine(["plugin","zip",buildDirArg]);
+	const uploader = async () => {
+		let url = ''
+		const listeners = {
+			stdout: (data) => {
+				data = data.toString();
+				if( data.startsWith('Upload completed')){
+					url = data.replace('Upload completed', '').trim();
+				}
+				if( data.startsWith('\u001b[32mUpload completed')){
+					url = data.replace('\u001b[32mUpload completed','').trim();
+				}
+
+			},
+			stderr: (data) => {
+
+			}
+		};
+		await runCommand({
+			path:paths.npx,
+			args:["plugin-machine", "upload", pluginDirArg, tokenArg],
+			options: {
+				listeners
+			}
+		});
+		return url;
+	}
+  	const upload = await uploader();
+	console.log({upload});
+  	core.setOutput('upload', upload);
 
 }
 
